51d11c83d3610b54fbcc2f79c662a5afd1ee8137
Ed Navarre <enavarr@siue.edu>
2022-03-01 19:13:20 -0600

    cleaned up

--- balance_read_mettler.py
+++ balance_read_mettler.py
@@ -10,7 +10,37 @@
 import serial
 import time
 import re
-#import xlwings as xw  #not needed for LibreOffice
+
+def get_mass():
+#    ser = serial.Serial(port='COM1',  ## if using Windows serial
+    ser = serial.Serial(port='/dev/ttyS0',
+                        baudrate=9600,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        bytesize=serial.EIGHTBITS
+    )
+
+    if not ser.isOpen():
+        ser.open()
+    
+    ser.write(b'\nSI\n')
+    time.sleep(1)  #give moment for balance to settle
+    
+    value = ser.read(ser.in_waiting())
+    value = value.decode('utf-8')
+    value = value.split('\n')[1][:-1]
+
+    if value[3] == 'S':
+        stability = True
+    else:
+        stability = False
+    weight = value[4:-1].strip(' ')
+
+    if ser.isOpen():
+        ser.close()
+        
+    return(weight)
+g_exportedScripts = (get_mass,)
 
 ##def read_weight(socket, timelapse=1):
 ##    """
@@ -33,42 +63,3 @@ import re
 ##    weight = value[4:-1].strip(' ')
 ##    return (weight, stability)
 ##
-
-def get_mass():
-
-#    ser = serial.Serial(port='COM1',
-    ser = serial.Serial(port='/dev/ttyS0',
-                        baudrate=9600,
-                        parity=serial.PARITY_NONE,
-                        stopbits=serial.STOPBITS_ONE,
-                        bytesize=serial.EIGHTBITS
-    )
-
-    if not ser.isOpen():
-        ser.open()
-    
-    ser.write(b'\nSI\n')
-    time.sleep(1)
-    #TODO check inWaiting length
-    value = ser.read(ser.inWaiting())
-    value = value.decode('utf-8')
-    value = value.split('\n')[1][:-1]
-
-    if value[3] == 'S':
-        stability = True
-    else:
-        stability = False
-    weight = value[4:-1].strip(' ')
-    print(weight)
-
-    #weight, stability = read_weight(ser)
-    #wb = xw.Book.caller() #xlwings not needed with LibreOffice
-    #wb.app.selection.value = weight #xlwings not needed with LibreOffice
-    #print('M = ' + str(weight) + ' g')
-
-    if ser.isOpen():
-        ser.close()
-        
-    return(weight)
-g_exportedScripts = (get_mass,)
-
